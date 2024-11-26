2e4a8f8dc03627fd728038404be2579dfa6914fd
Henry <henrysandsgrant@gmail.com>
2024-11-26 23:14:02 +0000

    Adding Question model to admin GUI

--- tt_backend/trivia/admin.py
+++ tt_backend/trivia/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'category', 'correct_answer')
+    search_fields = ('question', 'category')
+    list_filter = ('category',)
+
+# Register with customization
+admin.site.register(Question, QuestionAdmin)
